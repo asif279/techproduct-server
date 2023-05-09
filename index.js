a85746e71b5113ba6719cf256ad02ddbd4798a29
asif279 <asifofficial8130@gmail.com>
2023-05-09 22:39:30 +0600

    asof

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 const express = require('express');
+var cors = require('cors')
 require('dotenv').config();
 
 const app = express()
+app.use(cors())
+app.use(express.json())
 const port = 5000
-
+//${process.env.DB_Name}
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ia6ici6.mongodb.net/${process.env.DB_Name}?retryWrites=true&w=majority`;
-console.log(uri);
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.snelam0.mongodb.net/?retryWrites=true&w=majority`;
+
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("techProduct").collection("tech");
-  console.log("Database connected")
+// client.connect(err => {
+//   const collection = client.db("techProduct").collection("tech");
+//   console.log("Database connected")
   
-});
+// });
+
+async function run(){
+    try{
+ const serviceCollection = client.db("displayData").collection("data");
+ app.get('/data',async(req,res)=>{
+    const query ={};
+    const cursor=serviceCollection.find(query);
+    const data =await cursor.toArray();
+    const count= await serviceCollection.estimatedDocumentCount();
+    res.send(data);
+
+ })
+    }
+    finally{
+
+    }
+
+}
+run().catch(err=>console.error(err,"fuck "))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
 
 
 app.listen(port, () => {
